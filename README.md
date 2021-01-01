0910c5f9f69647f2876cc686f844052985c24c37
Scott Newcomer <snewcomer24@gmail.com>
2020-12-31 22:26:43 -0600

    Add -sync and improve tests

--- README.md
+++ README.md
@@ -4,8 +4,14 @@ Execute a command N times
 
 ## Usage
 
+Sends command to child process.  This starts subshells in parallel.
 ```
 $ ntimes 100 -- curl 'https://google.com' -s -o /dev/null -w "%{starttransfer}\n"
+```
+
+To send a sync process and wait for each subsequent command to finish, pass -sync
+```
+$ ntimes 100 -sync -- curl 'https://google.com' -s -o /dev/null -w "%{starttransfer}\n"
 ```
 
 ## License
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,59 @@
 use std::process::Command;
 use std::error::Error;
 use std::process;
+use std::io::{self, Write};
 
 /**
+ * parallel
  * ntimes 100 -- curl 'https://..'
+ *
+ * sync
+ * ntimes 100 -sync -- curl 'https://..'
  */
 fn main() {
     let args: Vec<String> = std::env::args().skip(1).collect();
 
-    let config: Config = Config::new(args);
-
-    if let Err(e) = ntimes(config.count, &config.cmd_name, &config.positional_args) {
-        // ensure if redirect to a new file that error shows up onscreen
-        eprintln!("Error {}", e);
+    let config: Config = Config::new(args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing command line args: {}", err);
         process::exit(1);
+    });
+
+    let mut cmd = Command::new(config.cmd_name);
+    cmd.args(config.positional_args);
+
+    if config.command_args.first() == Some(&"-sync".to_string()) {
+        if let Err(e) = ntimes_sync(config.count, &mut cmd) {
+            // ensure if redirect to a new file that error shows up onscreen
+            eprintln!("Error processing sync reqeusts: {}", e);
+            process::exit(1);
+        }
+    } else {
+        if let Err(e) = ntimes_parallel(config.count, &mut cmd) {
+            // ensure if redirect to a new file that error shows up onscreen
+            eprintln!("Error processing parallel reqeusts: {}", e);
+            process::exit(1);
+        }
     }
 }
 
-fn ntimes(count: usize, cmd_name: &str, positional_args: &[String]) -> Result<(), Box<dyn Error>> {
-    let mut cmd = Command::new(cmd_name);
-    cmd.args(positional_args);
-
+fn ntimes_parallel(count: usize, cmd: &mut Command) -> Result<(), Box<dyn Error>> {
     for _i in 0..count {
-        // child process concurrent.  spawn give stdout and stderror to parent process
+        // child process parallel.  spawn give stdout and stderror to parent process
         cmd.spawn()?;
+    }
+
+    Ok(())
+}
+
+fn ntimes_sync(count: usize, cmd: &mut Command) -> Result<(), Box<dyn Error>> {
+    for _i in 0..count {
+        // child process sync. stdout and stderr are captured by this child process
+        let output = cmd.output()?;
+
+        io::stdout().write_all(&output.stdout).unwrap();
+        io::stderr().write_all(&output.stderr).unwrap();
+
+        assert!(output.status.success());
     }
 
     Ok(())
@@ -38,25 +68,29 @@ struct Config {
 }
 
 impl Config {
-    pub fn new(args: Vec<String>) -> Self {
+    pub fn new(args: Vec<String>) -> Result<Config, &'static str> {
         let mut iter = args.iter();
 
         // we consumed `--` so it wont show up in positional_args
         let command_args: Vec<String> = iter.by_ref().take_while(|input| *input != "--").cloned().collect();
-        assert!(command_args.len() > 0, "Must provide number of times you want this comand to run");
+        if command_args.len() < 1 {
+            return Err("Must provide number of times you want this comand to run");
+        }
 
         let positional_args: Vec<String> = iter.cloned().collect();
-        assert!(positional_args.len() > 0, "Positional args after -- were not provided");
+        if positional_args.len() < 1 {
+            return Err("Positional args after `--` were not provided");
+        }
 
         let count: usize = command_args[0].parse().expect("A number was not provided");
         let cmd_name = &positional_args[0];
 
-        Self {
+        Ok(Self {
             cmd_name: cmd_name.clone(),
             count,
             command_args: command_args[1..].to_owned(),
             positional_args: positional_args[1..].to_owned(),
-        }
+        })
     }
 }
 
@@ -69,9 +103,16 @@ mod tests {
     }
 
     #[test]
+    fn ntimes_parallel_works() {
+        let mut cmd = Command::new("true");
+        let result = ntimes_parallel(2, &mut cmd);
+        assert!(result.is_ok());
+    }
+
+    #[test]
     fn config_works() {
         let v = vec_of_strings!["2", "--", "curl", "https://"];
-        let config = Config::new(v);
+        let config = Config::new(v).unwrap();
         assert_eq!(config.cmd_name, "curl".to_string());
         assert_eq!(config.count, 2);
         assert_eq!(config.positional_args, vec_of_strings!["https://"]);
@@ -82,6 +123,6 @@ mod tests {
     #[should_panic]
     fn config_errors_without_args() {
         let v = vec_of_strings!["2", "--"];
-        let config = Config::new(v);
+        Config::new(v).unwrap();
     }
 }
